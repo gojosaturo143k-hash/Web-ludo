6ea21e2af26d010648c729dc56d764739116c618
gojosaturo143k-hash <gojosaturo143k@gmail.com>
2026-01-28 05:22:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+const room = prompt("Enter room code");
+const ws = new WebSocket("ws://YOUR_SERVER_IP:8765");
+
+ws.onopen = () => {
+  ws.send(JSON.stringify({
+    type: "join",
+    room: room
+  }));
+};
+
+ws.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+
+  if (data.type === "info") {
+    alert(data.msg);
+  }
+
+  if (data.type === "chat") {
+    console.log("MSG:", data.msg);
+  }
+};
+
 document.getElementById("roll").onclick = () => {
-  alert("Dice rolled 🎲 (abhi fake)");
+  ws.send(JSON.stringify({
+    type: "message",
+    msg: "Dice rolled 🎲"
+  }));
 };
