73a93ae3c69f6dd9ad07ac5c51133b40800ac495
gojosaturo143k-hash <gojosaturo143k@gmail.com>
2026-01-28 07:04:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://10.117.14.150:8765");
+const ws = new WebSocket("wss://ido.onrender.com");
 
 const player = prompt("Enter your name");
 const room = prompt("Room code");
